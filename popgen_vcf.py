0aefcd38d8c6403b208cb16dfcc105cb1da817cf
Padraic Corcoran <padraiccorcoran1@gmail.com>
2016-04-24 14:35:55 +0100

    Updated command line args

--- popgen_vcf.py
+++ popgen_vcf.py
@@ -1,129 +1,187 @@
+from __future__ import print_function
 import sys
 import argparse
 import cyvcf
 import pgstats as pg
-import gzip
+import numpy as np
+import datetime
+
 
 def calc_rac(snp):
-     rac = (snp.num_hom_ref * 2) + snp.num_het
+    rac_num = (snp.num_hom_ref * 2) + snp.num_het
 
-     return rac
+    return rac_num
 
-parser = argparse.ArgumentParser(description="Program to calculate population genetic statistics from a region in VCF file")
-parser.add_argument('-i', '--vcf', required=True, dest='vcf_infile', help="VCF input file (Needs to be filtered in some way. SNPs without PASS in filter field will be excluded by default, but may be included if the -u flag is used (see below))")
-#parser.add_argument('-o', '--out', required=False, dest='vcf_infile', default=stdout, help="File where the output is written (default is to write to stdout)")
-parser.add_argument('-u', '--unifiltered_sites', required=False, action='store_true', dest='unfiltered', help="VCF input file (Include sites with a PASS in the filter field of the VCF (default; False")
-#parser.add_argument('-x', '--pop_1', required=False, dest='pop1', help="Samples belonging to population 1")
-#parser.add_argument('-y', '--pop_2', required=False, dest='pop2', help="Samples belonging to population 2")
-#parser.add_argument('-r', '--region', required=False, dest='region', help="Region to calculate stats. Format is chromosome:start-end or file listing regions (or sites) (1-based coordinate system). If region is not given the stats will be calculated on the whole vcf file")
-parser.add_argument('-d', '--min_dp', required=False, dest='min_dp', type = int, help="Minimum depth per genotype")
-parser.add_argument('-D', '--max_dp', required=False, dest='max_dp', type = int, help="Maximum depth per genotype")    
+
+
+parser = argparse.ArgumentParser(description="Program to calculate population genetic statistics from a region "
+                                             "in VCF file")
+parser.add_argument('-i', '--vcf', required=True, dest='vcf_infile',
+                    help="VCF input file (Needs to be filtered in some way. SNPs without PASS in filter field will be"
+                         " excluded by default, but may be included if the -u flag is used (see below))")
+parser.add_argument('-o', '--out', required=True, dest='outfile', help="Outfile to write results")
+parser.add_argument('-d', '--min_mean_dp', required=False, dest='min_dp', type=int,
+                    help="Minimum mean depth across genotypes")
+parser.add_argument('-D', '--max_mean_dp', required=False, dest='max_dp', type=int,
+                    help="Maximum mean depth across genotypes")
+parser.add_argument('-L', '--chromosome_file', required=False, dest='chroms', type=str,
+                    help="File listing chromsomes to include.")
+
+# parser.add_argument('-u', '--unifiltered_sites', required=False, action='store_true', dest='unfiltered',
+#                     help="Option to include sites without a PASS in the filter field of the VCF (default; False)")
+# parser.add_argument('-x', '--pop_1', required=False, dest='pop1', help="Samples belonging to population 1")
+# parser.add_argument('-y', '--pop_2', required=False, dest='pop2', help="Samples belonging to population 2")
+# parser.add_argument('-r', '--region', required=False, dest='region', help="Region to calculate stats.
+#  Format is chromosome:start-end or file listing regions (or sites) (1-based coordinate system).
+#  If region is not given the stats will be calculated on the whole vcf file")
+
 args = parser.parse_args()
 
+with open(args.chroms, 'r') as chrom_file:
+    chrom_list = [x.rstrip() for x in chrom_file]
+
+
+print("Analysing the following chromosomes")
+print(chrom_list)
 
 min_dp = 1
 if args.min_dp:
-     min_dp = args.min_dp
+    min_dp = args.min_dp
 
 max_dp = None
 if args.max_dp:
-     max_dp = args.max_dp
-
-
-#vcf_infile = cyvcf.Reader(gzip.open(args.vcf_infile, 'r'))
+    max_dp = args.max_dp
 
 vcf_infile = cyvcf.Reader(open(args.vcf_infile, 'r'))
 
 samples = vcf_infile.samples
 
-n_total = 2*len(samples) # assumes diploid genotypes. 
+n_total = 2 * len(samples)  # assumes diploid genotypes.
 
+#pop1_index = []
+#pop2_index = []
 
-pop1_index = []
-pop2_index = []
-
-n1 = len(pop1_index)
-n2 = len(pop2_index)
-
+#n1 = len(pop1_index)
+#n2 = len(pop2_index)
 
 indels = 0
+extreme_depth = 0
 low_call_rate = 0
+repeat_sites = 0
 spanning_deletion = 0
+snp_not_S = 0
 multiallelic_snp = 0
 valid_sites = 0
-failed_snp= 0
+failed_snp = 0
 ref_N = 0
 
-rac_list = [] # list to hold reference allele counts from SNP sites
+rac_list = []  # list to hold reference allele counts from SNP sites
+
+site_counter = 0
+
+print("Chromosome", "Position", "sites_analysed", "Date time")
 
 for site in vcf_infile:
-#for site in vcf_reader.fetch(chrom, start - 1, end):
-     if site.REF == 'N':
-          #ref_N += 1
-          continue
+        
+    if site.CHROM not in chrom_list:
+        continue
 
-     if site.is_indel:
-          #indels += 1
-          continue
-      
-     if len(site.ALT) >= 1 and site.ALT[-1] == '*': # SNPs at spanning deletion
-          #spanning_deletion += 1
-          continue
-     
-     all_DP =[x['DP'] for x in site.samples] # get the genotype depths
-     
-     
-     if 0 in all_DP or site.call_rate < 1.0: # only consider sites where all samples have coverage
-         #low_call_rate += 1
-         continue
+    site_counter += 1
 
-     if site.is_monomorphic:
-          valid_sites += 1
-          continue
+    if site_counter % 100000 == 0:
+        print(site.CHROM, site.POS, site_counter, datetime.datetime.now())
 
-     if site.is_snp:
-          if len(site.ALT) >  1:
-               #multiallelic_snp += 1
-               continue 
-          if site.aaf == 1.0 or site.aaf == 0.0: # only want SNPs polymorphic in ou\r sample                         
-               valid_sites += 1
-               continue
+    if site.REF == 'N':
+        ref_N += 1
+        continue
 
-          if not args.unfiltered:
-               if site.FILTER == 'PASS':
-                    rac = calc_rac(site)
-                    rac_list.append(rac)
-                    valid_sites += 1
-                    continue
-               else:
-                    #failed_snp += 1
-                    continue
-          else:
-               rac = calc_rac(site)
-               ral_list.append(site)
-               valid_sites += 1
-               continue
-     else:          
-          problem_site = site.CHROM + '\t' + str(site.POS)
-          error_message = 'Could not assign ' + problem_site + ' to site type'
-          sys.exit(error_message)
-                    
+    if site.is_indel:
+        indels += 1
+        continue
+
+    if len(site.ALT) >= 1 and site.ALT[-1] == '*':  # SNPs at spanning deletion
+        spanning_deletion += 1
+        continue
+
+    all_DP = [x['DP'] for x in site.samples]  # get the genotype depths
+
+    if None in all_DP:  # Exclude sites where a genotype DP field is set to '.'
+        low_call_rate += 1
+        continue
+
+    mean_DP = np.nanmean(all_DP)
+
+    if mean_DP < args.min_dp or mean_DP > args.max_dp: # depth filter
+        extreme_depth += 1
+        continue
+
+    if 0 in all_DP or site.call_rate < 1.0:  # only consider sites where all samples have coverage
+        low_call_rate += 1
+        continue
+
+    if site.FILTER == "REPEAT" or "REPEAT" in site.FILTER:
+        repeat_sites += 1
+        continue
+
+    if site.is_monomorphic:
+        valid_sites += 1
+        continue
+
+    if site.is_snp:
+        if len(site.ALT) > 1:
+            multiallelic_snp += 1
+            continue
+        if site.aaf == 1.0 or site.aaf == 0.0:  # only want SNPs polymorphic in our sample
+            valid_sites += 1
+            if site.aaf == 1.0:
+                snp_not_S += 1 # A SNP different from reference, but not segregating in the samples
+            continue
+
+        # if not args.unfiltered:
+        if site.FILTER == 'PASS':
+            rac = calc_rac(site)
+            rac_list.append(rac)
+            valid_sites += 1
+            continue
+        else:
+            failed_snp += 1
+            continue
+        # else:
+        #     rac = calc_rac(site)
+        #     rac_list.append(rac)
+        #     valid_sites += 1
+        #     continue
+    else:
+        problem_site = site.CHROM + '\t' + str(site.POS)
+        error_message = 'Could not assign ' + problem_site + ' to site type'
+        sys.exit(error_message)
 
 S = len(rac_list)
-theta_w = pg.thetaW(n_total, rac_list)
+theta_w = pg.thetaW(n_total, S)
 pi = pg.pi(n_total, rac_list)
-tajD = pg.TajimasD(n_total, S, theta_w, pi)
+tajD = round(pg.TajimasD(n_total, S, theta_w, pi), 5)
+
+theta_w_site = round(theta_w / float(valid_sites), 5)
+theta_w_percent = theta_w_site * 100
+
+pi_site = round(pi / float(valid_sites), 5)
+pi_percent = pi_site * 100
 
 
-print 'S:', S
-print 'thetaW:', theta_w / float(valid_sites)
-print 'pi:', pi / float(valid_sites)
-print "Tajima's D:", tajD
-print 'valid sites:', valid_sites
-print 'ref N:', ref_N
-print 'indels:', indels 
-print 'snp spanning deletion:', spanning_deletion
-print 'low call rate:', low_call_rate
-print 'multiallelic snps:', multiallelic_snp 
-print 'failed biallelic snps:', failed_snp
-print 'total sites considered:', valid_sites + indels + spanning_deletion + low_call_rate + multiallelic_snp + failed_snp
+with open(args.outfile, 'w') as outfile:
+    print(args.vcf_infile, file=outfile)
+    print('S:', S, file=outfile)
+    print('thetaW per site (%):', theta_w_site, '({0})'.format(str(theta_w_percent)), file=outfile)
+    print('pi per site (%):', pi_site, '({0})'.format(str(pi_percent)), file=outfile)
+    print("Tajima's D:", tajD, file=outfile)
+    print('valid sites:', valid_sites, file=outfile)
+    print('ref N:', ref_N, file=outfile)
+    print('indels:', indels, file=outfile)
+    print('snp spanning deletion:', spanning_deletion, file=outfile)
+    print('low call rate:', low_call_rate, file=outfile)
+    print('Sites with extreme depth', extreme_depth, file=outfile)
+    print('Sites in repeat regions', repeat_sites, file=outfile)
+    print('multiallelic snps:', multiallelic_snp, file=outfile)
+    print('failed biallelic snps:', failed_snp, file=outfile)
+    print('total sites considered:', valid_sites + ref_N + indels + spanning_deletion + low_call_rate + extreme_depth +
+          repeat_sites + multiallelic_snp + failed_snp, file=outfile)
